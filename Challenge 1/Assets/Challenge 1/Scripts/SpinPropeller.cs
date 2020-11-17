cd8953037ad3a6f68d31d3ad864aa9d76e8ab7c4
icxrus <emppu.hietikko@gmail.com>
2020-11-17 14:04:08 +0200

    First post of Challenge 1 and Prototype 1 & 2
    Finished Challenge 1 and Prototype 1, in progress Prototype 2.

---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+
+    public float rotationSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotates the propeller on the plane
+        transform.Rotate(Vector3.back * rotationSpeed);
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float speed = 25.0f;
+    private float turnSpeed = 15.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Forward and Horizontal movement based on player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
