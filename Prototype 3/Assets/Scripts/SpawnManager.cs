a61b384332a8f4dedc122ba0210446a5e3d57faa
icxrus <emppu.hietikko@gmail.com>
2020-11-19 12:34:25 +0200

    Finished Prototype 3
    Started and finished prototype 3

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatDelay = 2;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Repeat obstacle spawn
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerController>();
+
+    }
+
+    // Stop spawning obstacle if game is over
+    void SpawnObstacle () {
+        if (playerControllerScript.gameOver == false) {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
