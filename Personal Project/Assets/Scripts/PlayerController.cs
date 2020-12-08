2c141e2e2a918d2601151c251035b7a3ea41942e
icxrus <emppu.hietikko@gmail.com>
2020-12-08 14:09:48 +0200

    Started Personal Game Project and added gitignore to previous files missing them

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject Planet;
+    public GameObject PlayerPlaceholder;
+
+
+    public float speed;
+    public float turnSpeed;
+    public float JumpHeight = 1.2f;
+
+    float gravity = 10;
+    bool OnGround = false;
+
+
+    float distanceToGround;
+    Vector3 Groundnormal;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+
+
+        //Jump
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * 20 * JumpHeight * Time.deltaTime);
+
+        }
+
+
+
+        //GroundControl
+
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+
+            distanceToGround = hit.distance;
+            Groundnormal = hit.normal;
+
+            if (distanceToGround <= 0.2f)
+            {
+                OnGround = true;
+            }
+            else
+            {
+                OnGround = false;
+            }
+
+
+        }
+
+
+        //GRAVITY and ROTATION
+
+        Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
+
+        if (OnGround == false)
+        {
+            rb.AddForce(gravDirection * -gravity);
+
+        }
+
+        //
+
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, Groundnormal) * transform.rotation;
+        transform.rotation = toRotation;
+
+
+
+    }
+
+
+   
+
+
+}
